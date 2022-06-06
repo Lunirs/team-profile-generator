6883d95a130b4158074c3e42ae9dd6e06c8827b1
Lunirs <dhong0925@gmail.com>
2022-06-06 17:31:53 -0400

    Added test cases for employee.js

---
+++ tests/employee.test.js
@@ -0,0 +1,53 @@
+const Employee = require("../lib/employee");
+
+describe("Employee", () => {
+  it("should create a new employee object", () => {
+    const newEmployee = new Employee();
+    expect(typeof newEmployee).toEqual("object");
+  });
+
+  it("should be able to set a name value when passed in as a constructor argument", () => {
+    const employeeName = "Daniel";
+    const newEmployee = new Employee(employeeName);
+    expect(newEmployee.name).toEqual("Daniel");
+  });
+
+  it("should be able to set a id value when passed in as a constructor argument", () => {
+    const employeeId = "US111111";
+    const newEmployee = new Employee(employeeId);
+    expect(newEmployee.employeeId).toEqual("US111111");
+  });
+
+  it("should be able to set an email value when passed in as a constructor argument", () => {
+    const employeeEmail = "something@gmail.com";
+    const newEmployee = new Employee(employeeEmail);
+    expect(newEmployee.employeeEmail).toEqual("something@gmail.com");
+  });
+
+  describe("getName", () => {
+    it("should be able to retrieve the employee's name value from an employee object", () => {
+      const nameVal = "Daniel";
+      const newEmployee = new Employee(nameVal);
+
+      expect(newEmployee.getName()).toEqual(nameVal);
+    });
+  });
+
+  describe("getId", () => {
+    it("should be able to retrieve the employee's id value from an employee object", () => {
+      const idVal = "US111111";
+      const newEmployee = new Employee("Daniel", idVal);
+
+      expect(newEmployee.getID()).toEqual(idVal);
+    });
+  });
+
+  describe("getEmail", () => {
+    it("should be able to retrieve the employee's email value from an employee object", () => {
+      const emailVal = "something@gmail.com";
+      const newEmployee = new Employee("Daniel", "US111111", emailVal);
+
+      expect(newEmployee.getEmail()).toEqual(emailVal);
+    });
+  });
+});
