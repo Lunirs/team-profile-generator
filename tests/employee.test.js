506518b9e2dd2d6d0d31d45f0b1cbeb7b1aadecb
Lunirs <dhong0925@gmail.com>
2022-06-06 17:55:23 -0400

    Added engineer test and added new test to employee test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -9,19 +9,19 @@ describe("Employee", () => {
   it("should be able to set a name value when passed in as a constructor argument", () => {
     const employeeName = "Daniel";
     const newEmployee = new Employee(employeeName);
-    expect(newEmployee.name).toEqual("Daniel");
+    expect(newEmployee.name).toEqual(employeeName);
   });
 
   it("should be able to set a id value when passed in as a constructor argument", () => {
     const employeeId = "US111111";
     const newEmployee = new Employee(employeeId);
-    expect(newEmployee.employeeId).toEqual("US111111");
+    expect(newEmployee.employeeId).toEqual(employeeId);
   });
 
   it("should be able to set an email value when passed in as a constructor argument", () => {
     const employeeEmail = "something@gmail.com";
     const newEmployee = new Employee(employeeEmail);
-    expect(newEmployee.employeeEmail).toEqual("something@gmail.com");
+    expect(newEmployee.employeeEmail).toEqual(employeeEmail);
   });
 
   describe("getName", () => {
@@ -48,6 +48,19 @@ describe("Employee", () => {
       const newEmployee = new Employee("Daniel", "US111111", emailVal);
 
       expect(newEmployee.getEmail()).toEqual(emailVal);
+    });
+  });
+
+  describe("getEmployeeTitle", () => {
+    it("should be able to retrieve the job title of the employee", () => {
+      const title = "Employee";
+      const newEmployee = new Employee(
+        "Daniel",
+        "US111111",
+        "something@gmail.com"
+      );
+
+      expect(newEmployee.getEmployeeTitle()).toEqual(title);
     });
   });
 });
