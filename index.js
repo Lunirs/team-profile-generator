ff202c423d11e260c497b9bcf6562670e475de40
Lunirs <dhong0925@gmail.com>
2022-06-06 20:30:59 -0400

    Added second prompt extension for the additional parameter for each different role

--- index.js
+++ index.js
@@ -6,27 +6,52 @@ const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 
 const addEmployee = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Please enter your team member's name",
-    },
-    {
-      type: "list",
-      name: "role",
-      message: "What job title does this team member have?",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Please input your team member's id number",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please input your team member's email address",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter your team member's name",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What job title does this team member have?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please input your team member's id number",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please input your team member's email address",
+      },
+    ])
+    .then(function ({ name, id, email, role }) {
+      let empParam = "";
+      if (role === "Manager") {
+        empParam === "Office Number";
+      } else if (role === "Engineer") {
+        empParam === "Github Username";
+      } else if (role === "Intern") {
+        empParam === "School's name";
+      }
+
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "empParam",
+          message: `Please input your team member's ${empParam}`,
+        },
+        {
+          type: "list",
+          name: "addMore",
+          message: "Do you have more team members to add?",
+          choices: ["Y", "N"],
+        },
+      ]);
+    });
 };
